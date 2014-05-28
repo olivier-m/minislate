f5e70fdc50a98daeb9b2d47413e0da46a406dea3
Olivier Meunier <olivier@neokraft.net>
2014-05-28 14:55:59 +0200

    Fixed some bugs with preformated text.

--- src/lib/controls/block.js
+++ src/lib/controls/block.js
@@ -25,7 +25,17 @@ var Block = Class(Button, {
     },
 
     click: function() {
+        var editor = this.toolbar.editor,
+            topNodes;
+
         if (this.command === 'formatblock') {
+            // Expand selection before formating to avoid some issues with webkit
+            if (!this.isHighlighted()) {
+                topNodes = editor.filterTopNodeNames.apply(editor, editor.BLOCK_NODES);
+                if (topNodes.length > 0) {
+                    editor.setRange(topNodes[0]);
+                }
+            }
             this.toolbar.editor.exec(this.command, '<' + this.tag + '>');
         } else {
             this.toolbar.editor.exec(this.command);
@@ -67,21 +77,35 @@ exports.Preformated = Class(Block, {
     defaults: extend({}, Block.prototype.defaults, {
         label: '<>',
         title: 'Code',
-        fontAwesomeID: 'code'
+        fontAwesomeID: 'code',
+        tabReplacement: '    '
     }),
 
     init: function() {
         Block.prototype.init.apply(this, arguments);
-        var editor = this.toolbar.editor;
+        var self = this,
+            editor = this.toolbar.editor;
 
         editor.on('keydown', function(evt) {
-            // Activate tab in preformated blocks
             if (evt.which === 9 && !evt.ctrlKey && !evt.metaKey && !evt.shiftKey) {
+                // Activate tab in preformated blocks
                 if (editor.filterTopNodeNames('pre').length > 0) {
                     evt.preventDefault();
-                    editor.exec('insertHtml', '    ');
+                    editor.exec('insertHtml', self.options.tabReplacement);
                 }
             }
+        });
+    },
+    click: function() {
+        // Replace <br> nodes by new line after formating (another webkit BS)
+        Block.prototype.click.call(this);
+        var editor = this.toolbar.editor,
+            node = editor.getEnclosingNode(),
+            nodeList = [].slice.call(node.getElementsByTagName('br'));
+
+        _.each(nodeList, function(n) {
+            n.parentNode.insertBefore(document.createTextNode('\n'), n);
+            n.parentNode.removeChild(n);
         });
     }
 });
